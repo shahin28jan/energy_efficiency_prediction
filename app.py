498ecca00716c5537460bd209f164ac985cb2a5d
shahin28jan <shahinalwaz@gmail.com>
2023-04-29 13:27:51 +0530

    data_ingestion.py data_transformation

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask,request,render_template,jsonify
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+
+
+application=Flask(__name__)
+
+app=application
+
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['GET','POST'])
+
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('form.html')
+    
+    else:
+        data=CustomData(
+            relative_compactness=float(request.form.get('relative_compactness')),
+            surface_area=float(request.form.get('surface_area')),
+            wall_area = float(request.form.get('wall_area')),
+            roof_area = float(request.form.get('roof_area')),
+            overall_height = float(request.form.get('overall_height')),
+            glazing_area = float(request.form.get('glazing_area')),
+            glazing_area_distribution = float(request.form.get('glazing_area_distribution')),
+            
+        )
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictPipeline()
+        pred=predict_pipeline.predict(final_new_data)
+
+        result=round(pred[0],2)
+
+        return render_template('result.html',final_result=result)
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,66 @@
+import sys
+import os
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+import pandas as pd
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join('artifacts','model.pkl')
+
+            preprocessor=load_object(preprocessor_path)
+            model=load_object(model_path)
+
+            data_scaled=preprocessor.transform(features)
+
+            pred=model.predict(data_scaled)
+            return pred
+            
+
+        except Exception as e:
+            logging.info("Exception occured in prediction")
+            raise CustomException(e,sys)
+       
+        
+class CustomData:
+    def __init__(self,
+                 relative_compactness:float,
+                 surface_area:float,
+                 wall_area:float,
+                 roof_area:float,
+                 overall_height:float,
+                 glazing_area:float,
+                 glazing_area_distribution:float):
+        
+        self.relative_compactness=relative_compactness
+        self.surface_area=surface_area
+        self.wall_area=wall_area
+        self.roof_area=roof_area
+        self.overall_height=overall_height
+        self.glazing_area = glazing_area
+        self.glazing_area_distribution = glazing_area_distribution
+        
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'relative_compactness':[self.relative_compactness],
+                'surface_area':[self.surface_area],
+                'wall_area':[self.wall_area],
+                'roof_area':[self.roof_area],
+                'overall_height':[self.overall_height],
+                'glazing_area':[self.glazing_area],
+                'glazing_area_distribution':[self.glazing_area_distribution]
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
